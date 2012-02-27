413187a8457e9d86ff2e97a10171830e79d1be7d
“Carl <carlcrott@gmail.com>
2012-02-27 14:31:30 -0500

    lolz switched from RJB to JRuby lib

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler'
 require 'pathname'
+require 'java'
+
 
 Bundler.require
 
@@ -15,44 +17,43 @@ end
 
 
 get '/parse' do
-
-  def oscar_parse
-
+  def oscarizer
     this_dir = Pathname.new(File.dirname(__FILE__))
 
-    jars = [
-      "#{this_dir}/jars/commons-lang-2.1.jar", 
-      "#{this_dir}/oscar4-all-4.1-with-dependencies.jar", 
+    # specify individual .jars used in this project
+    class_paths = [
+      "commons-lang-2.1.jar",
+      "oscar4-all-4.1-with-dependencies.jar"
     ]
 
-    Rjb::load(classpath = jars.join(':'), jvmargs=[])
+    for class_path in class_paths
+      jar_path = (this_dir + class_path)
+      $CLASSPATH << jar_path
+    end
 
-    name = "To a solution of 3-bromobenzophenone (1.00 g, 4 mmol) in MeOH (15 mL) was added sodium borohydride (0.3 mL, 8 mmol) portionwise at rt and the suspension was stirred at rt for 1-24 h. The reaction was diluted slowly with water and extracted with CH2Cl2. The organic layer was washed successively with water, brine, dried over Na2SO4, and concentrated to give the title compound as oil (0.8 g, 79%), which was used in the next reaction without further purification. MS (ESI, pos. ion) m/z: 247.1 (M-OH)."
+    include_class "uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.ChemicalStructure"
+    include_class "uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.FormatType"
+    include_class "uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.ResolvedNamedEntity"
+    include_class "uk.ac.cam.ch.wwmm.oscar.Oscar"
 
-    oscar = Rjb::import('uk.ac.cam.ch.wwmm.oscar.Oscar').new
-    format = Rjb::import('uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.FormatType').INCHI
-    inchi = Rjb::import('uk.ac.cam.ch.wwmm.oscar.chemnamedict.entities.ChemicalStructure')
+    tester = "To a solution of 3-bromobenzophenone (1.00 g, 4 mmol) in MeOH (15 mL) was added sodium borohydride (0.3 mL, 8 mmol) portionwise at rt and the suspension was stirred at rt for 1-24 h. The reaction was diluted slowly with water and extracted with CH2Cl2. The organic layer was washed successively with water, brine, dried over Na2SO4, and concentrated to give the title compound as oil (0.8 g, 79%), which was used in the next reaction without further purification. MS (ESI, pos. ion) m/z: 247.1 (M-OH)."
 
-    entities = oscar.findAndResolveNamedEntities(name).toArray
+    oscar = Oscar.new
 
-    codified = []
+    entities = oscar.findAndResolveNamedEntities(tester)
 
     for ne in entities
       puts ne.getSurface()
-      codified << ne.getSurface()
-
-      inchi = ne.getFirstChemicalStructure(format);
-      if inchi.methods.include?('toString') == true 
-        puts inchi.toString
-        codified << inchi.toString
-      end
+      inchi = ne.getFirstChemicalStructure(FormatType::INCHI)
+      inchi != nil ? (puts inchi) : ""
     end
 
-    # can return this as JSON
-    return codified.join("***")
   end
 
-  "#{oscar_parse}"
+  
+
+
+  "#{oscarizer}"
   
 
 end
